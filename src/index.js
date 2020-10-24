f7057c88325ccdc942a049aa2d162324cead1d00
mxwrlld <maksim.mixwell@gmail.com>
2020-10-24 11:40:56 +0500

    init: solving the problem of reversing the number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0)
+        n *= -1;
+    let res = String(n).split('').reverse().join('');
+    return  Number(res.length < 3 ? res += '0': res += '');
 }
